ef431873237ece07eaae55d01a838b06ba0b7637
赵立栋 <81725492+zhaolidong0608@users.noreply.github.com>
2026-03-20 14:28:21 +0800

    V1

---
+++ demo/config.py
@@ -0,0 +1,4 @@
+API_KEY = "7b3a26c97b1ffc0088481f0e476b3bea9850db78bd49b3d80e7119062abe74be51c4ce605ca7fb109894168b9325df9fc3e7eac037b82a197986b634d76171754e20e23a7b15d934db103f96578568b5"
+SECRET_KEY = "49c1f998f2d4e74a716785f2a9f50a96057e4c40"
+API_URL = "https://01bot.baidu.com/api/ly_llm/v1/chat/completions"
+AUTH_URL = "https://aip.baidubce.com/oauth/2.0/token"
